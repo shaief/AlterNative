6605789d70156080b78388fa7e08a0ffee5e8f12
shaief <shaief@gmail.com>
2014-11-20 15:15:41 +0200

    playing with js. ignore.

--- client/main.js
+++ client/main.js
@@ -1,5 +1,3 @@
-// counter starts at 0
-
 Template.main.helpers({
     from_location: function () {
         if (Session.get("from-location")) {
@@ -9,9 +7,30 @@ Template.main.helpers({
 });
 
 Template.main.events({
-    'keyup .from-location': function () {
-        Session.set(".google-json-api", Session.get(".from-location"));
+    'keyup .from-location': function (e) {
+        Session.set(".google-json-api", $(".from-location").val());
+        console.log("from location key up");
+        if (e.which == 13) {
+            console.log('You pressed enter!');
+        }
     },
+
+    'keyup .to-location': function (e) {
+        Session.set(".google-json-api", $(".from-location").val());
+        console.log("to location key up");
+        if (e.which == 13) {
+            console.log('You pressed enter!');
+        }
+    },
+
+    'onclick .time': function () {
+        $('.time').toggleClass('active');
+    },
+
+    'onclick .money': function () {
+        $('.money').toggleClass('active');
+    },
+
     'click .not-eco': function () {
         console.log('not-eco');
         $('.not-eco').toggleClass('btn-danger btn-default');
