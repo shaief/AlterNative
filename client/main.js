2fe665f442f425a500ddb216ddc2c94c665583ce
yonatan braude <yonatan.py@gmail.com>
2014-11-20 15:12:22 +0200

    distances is set in Session.get(\\distances\)

--- client/main.js
+++ client/main.js
@@ -32,16 +32,9 @@ Template.main.events({
         $('.eco').toggleClass('btn-success btn-default');
         $('.eco').toggleClass('active');
         $('.not-eco').toggleClass('active');
+    },
+    'click .know': function(){
+        console.log(setDistanceMatric())
     }
 });
 
-setAutoComplete = function (){
-    var from = document.getElementsByClassName('from-location')[0];
-    var to = document.getElementsByClassName('to-location')[0];
-    var options = {
-      componentRestrictions: {country: 'il'},
-      types: []
-    };
-    new google.maps.places.Autocomplete(from, options);
-    new google.maps.places.Autocomplete(to, options);
-}
--- client/setPlacesSearch.js
+++ client/setPlacesSearch.js
@@ -8,3 +8,44 @@ setAutoComplete = function (){
     new google.maps.places.Autocomplete(from, options);
     new google.maps.places.Autocomplete(to, options);
 }
+
+setDistanceMatric = function () {
+	Session.set('distances', {});
+	setDistanceCar();
+	setDistanceWalking();
+}
+
+setDistanceCar = function (){
+	setDistanceByType(google.maps.TravelMode.DRIVING);
+}
+
+setDistanceWalking = function () {
+	setDistanceByType(google.maps.TravelMode.WALKING);	
+}
+
+setDistanceByType = function (type) {
+	var origin = $('.from-location').val();
+	var destination = $('.to-location').val();
+
+	var service = new google.maps.DistanceMatrixService();
+	service.getDistanceMatrix(
+	  {
+	    origins: [origin],
+	    destinations: [destination],
+	    travelMode: type,
+	    unitSystem: google.maps.UnitSystem.METRIC,
+	    durationInTraffic: false,
+	    avoidHighways: false,
+	    avoidTolls: false
+	  }, callback);
+
+	function callback(response, status) {
+		var element = response.rows[0].elements[0]
+	  	var distances = Session.get('distances');
+	  	distances[type] = {
+	  		duration: element.duration.value / 60,
+	  		distance: element.distance.value / 1000
+	  	}
+	  	Session.set('distances', distances);
+	}
+}
