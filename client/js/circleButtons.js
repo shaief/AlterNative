69edf8e9e22fbf4492f499a5a7a65abea1fb3478
shaief <shaief@gmail.com>
2014-11-22 17:44:03 +0200

    moving things to css. cleaning things up

--- client/js/circleButtons.js
+++ client/js/circleButtons.js
@@ -2,34 +2,46 @@ toggleCircle = function(className, effectFunction){
     $('.circle').not($('.' + className)).removeClass('active');
     $('.circle.' + className).toggleClass('active');
     effectFunction();
+
+    // remove all category classes
+    $('body').removeClass('ecology calories time money');
     $('h1').removeClass('ecology calories time money');
     $('h4').removeClass('ecology calories time money');
+
+    // add only relevant class
+    $('body').addClass(className);
     $('h1').addClass(className);
     $('h4').addClass(className);
     $('label').addClass(className);
+
     Session.set('sorter', className);
-}
+};
+
 toggleEcology = function () {
     toggleCircle('ecology', function(){
-        $('body').css("background", "green");    
+        console.log('ecology');
     });
 };
 
 toggleCalories = function () {
+//$('body').css("background", "green");
     toggleCircle('calories', function(){
-        $('body').css("background", "#fff");    
+        console.log('calories');
+        //$('body').css("background", "#fff");
     });
 };
 
 toggleTime = function () {
     toggleCircle('time', function(){
-        $('body').css("background-image", "url('Antique_mechanical_clock.jpg')");    
+        console.log('time');
+        //$('body').css("background-image", "url('Antique_mechanical_clock.jpg')");
     });
 };
 
 toggleMoney = function () {
     toggleCircle('money', function(){
-        $('body').css("background-image", "url('50_NIS_Bill.jpg')");    
+        console.log('money');
+        //$('body').css("background-image", "url('50_NIS_Bill.jpg')");
     });
 
 };
