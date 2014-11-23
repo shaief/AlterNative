87c314cbdf9eac497e23f8ec284aa9237afec59e
shaief <shaief@gmail.com>
2014-11-23 21:53:33 +0200

    refactoring. making it generic

--- client/js/know.js
+++ client/js/know.js
@@ -1,5 +1,17 @@
+toggleCircle(Session.get('sorter'));
+
 Template.know.events({
     'click .back': function () {
         moveToView('set-route');
+    }
+});
+
+Template.know.events({
+    'click .circle': function () {
+        $(".circle").click(function () {
+            var circleId = $(this).attr("id");
+            console.log('the ' + circleId + '  circle button was clicked');
+            toggleCircle(circleId);
+        });
     }
 });
