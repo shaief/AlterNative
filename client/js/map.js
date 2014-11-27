003af173dd5d1748da55d949101829e359ab8452
yonatan braude <yonatan.py@gmail.com>
2014-11-27 16:53:17 +0200

    Map!

--- client/js/map.js
+++ client/js/map.js
@@ -1,3 +1,36 @@
-/**
- * Created by jak on 27/11/14.
- */
+Template.map.rendered = function () {
+    initializeMap();
+    calcRoute();
+};
+
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
+var map;
+
+initializeMap = function () {
+    directionsDisplay = new google.maps.DirectionsRenderer();
+    var chicago = new google.maps.LatLng(41.850033, -87.6500523);
+    var mapOptions = {
+        zoom:7,
+        center: chicago
+    };
+    map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+    directionsDisplay.setMap(map);
+}
+
+calcRoute = function () {
+    var start = Session.get('from').formatted_address;
+    var end = Session.get('to').formatted_address;
+
+    var request = {
+        origin:start,
+        destination:end,
+        travelMode: google.maps.TravelMode.DRIVING
+    };
+    directionsService.route(request, function(result, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(result);
+        }
+    });
+}
