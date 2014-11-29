5dfef2b1eef41b289634a24464f2e3bb3c6ca63c
idoivri <idoivri@gmail.com>
2014-11-29 15:05:19 +0200

    Add calculations for taxi

--- client/lib/calculations.js
+++ client/lib/calculations.js
@@ -1,3 +1,5 @@
+alternativeTaxiString = "taxi";
+
 //Gets distances matrix with different transportation types, extends each object 
 //with calories and CO2 emmissions data
 
@@ -16,6 +18,23 @@ setDataDriving = function (response, status) {
 	};
 	Session.set('distances', distances);
 };
+
+setDataTaxi = function (response, status) {
+	var distances = Session.get('distances');
+	var element = response.rows[0].elements[0];
+	var price = 12.3 + (element.distance.value / 1000 * 0.7834) + (element.duration.value / 60 * 0.3);
+	distances[alternativeTaxiString] = {
+		duration: element.duration.value / 60,
+		distance: element.distance.value / 1000,
+		name: alternativeTaxiString.toLocaleLowerCase(),
+		type: alternativeTaxiString,
+		price: price,
+		emmissions: 271 * element.distance.value, //271g CO2 per KM
+		calories: 0
+	};
+	Session.set('distances', distances);
+};
+
 
 setDataWalking = function (response, status) {
 	var distances = Session.get('distances');
