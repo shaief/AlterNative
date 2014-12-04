10f59d01226e0f5dd01aecd222605318df6da164
shaief <shaief@gmail.com>
2014-12-04 09:29:35 +0200

    fix circle button event (more explicit)

--- client/lib/circleButtons.js
+++ client/lib/circleButtons.js
@@ -1,6 +1,6 @@
 toggleCircle = function(circleClass){
     //$('.circle').children().not($('span.' + circleClass)).removeClass('active');
-    $('button.circle').not('.circle .'+circleClass).removeClass('active');
+    $('button.circle').not('button.circle .'+circleClass).removeClass('active');
     $('.' + circleClass).parent('button.circle').toggleClass('active');
 
     // remove all category classes and add only relevant class
@@ -11,7 +11,7 @@ toggleCircle = function(circleClass){
 };
 
 circleClickHandler = function (jQueryEvent, BlazeTemplateInstance) {
-    var circleClass = $(jQueryEvent.target).attr('class');
+    var circleClass = $(jQueryEvent.target).children('img').attr('class');
     console.log('the ' + circleClass + '  circle button was clicked');
     toggleCircle(circleClass);
 };
